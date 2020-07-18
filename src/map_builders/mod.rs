710848bf85ba6a1064a35b98709809a8adbf9738
Jerome M. St.Martin <jeromemst.martin@gmail.com>
2020-07-18 11:30:33 -0700

    drunkard mapgen implemented

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -7,6 +7,8 @@ mod bsp_interior;
 use bsp_interior::BspInteriorBuilder;
 mod cellular_automata;
 use cellular_automata::CellularAutomataBuilder;
+mod drunkard;
+use drunkard::{DrunkardsWalkBuilder, /*DrunkardSettings, DrunkSpawnMode*/};
 mod common;
 use common::*;
 use specs::prelude::*;
@@ -22,11 +24,14 @@ pub trait MapBuilder {
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
     let mut rng = rltk::RandomNumberGenerator::new();
-    let builder = rng.roll_dice(1, 2);
+    let builder = rng.roll_dice(1, 7);
     match builder {
         1 => Box::new(SimpleMapBuilder::new(new_depth)),
         2 => Box::new(BspDungeonBuilder::new(new_depth)),
         3 => Box::new(BspInteriorBuilder::new(new_depth)),
-        _ => Box::new(CellularAutomataBuilder::new(new_depth))
+        4 => Box::new(CellularAutomataBuilder::new(new_depth)),
+        5 => Box::new(DrunkardsWalkBuilder::open_area(new_depth)),
+        6 => Box::new(DrunkardsWalkBuilder::open_halls(new_depth)),
+        _ => Box::new(DrunkardsWalkBuilder::winding_passages(new_depth)),
     }
 }
